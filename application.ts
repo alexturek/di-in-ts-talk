18d097be973267db0628d7a85a31c8c5d653ab5f
Alex Turek <alex@convoy.com>
2017-10-18 01:07:07 -0700

    DI the application and library

--- application.ts
+++ application.ts
@@ -1,17 +1,20 @@
 import { Band, Musician } from './types';
 import * as library from './library';
+import * as config from './config';
+import { Database } from './database';
 
 async function main() {
-  const server = buildServer();
+  const database = new Database(config.db);
+  const server = buildServer(database);
   server.listenForever(80);
 }
 
-function buildServer() {
+function buildServer(db: Database) {
   const server: any = null;
   server.post('/start-band', async (req: any, res: any) => {
-    const wilburys = await library.findBandsWith('bob dylan');
+    const wilburys = await library.findBandsWith(db, 'bob dylan');
     if (!wilburys) {
-      await library.startBand('heartbreakers', ['tom petty']);
+      await library.startBand(db, 'heartbreakers', ['tom petty']);
     }
     res.status(200).end();
   });
--- library.ts
+++ library.ts
@@ -1,16 +1,16 @@
 import * as _ from 'lodash';
 
 import { Band, Musician } from './types';
-import * as database from './database';
+import { Database } from './database';
 
-export async function findBandsWith(name: string): Promise<Musician[][]> {
+export async function findBandsWith(database: Database, name: string): Promise<Musician[][]> {
   // we only look at adults
   const users = await database.getMusiciansLike({ name, age: { $gte: 18 }});
   const bands = _.groupBy(users, 'bandId');
   return _.values(bands);
 }
 
-export async function startBand(bandName: string, musicianNames: string[]): Promise<[Band, Musician[]]> {
+export async function startBand(database: Database, bandName: string, musicianNames: string[]): Promise<[Band, Musician[]]> {
   let band: Band;
   let users: Musician[];
   await database.withTransaction(async () => {
